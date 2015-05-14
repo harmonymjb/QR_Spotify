35ff055099149a3b066a735c66bbe1fce4c6a6e9
omnistegan <gmbouvier@gmail.com>
2015-05-13 17:12:06 -0700

    Added function for checking if spotify is already running.

--- spotify_qdbus.py
+++ spotify_qdbus.py
@@ -1,10 +1,41 @@
 import os
+import subprocess
+import re
+
+
+def findProcess(name):
+    processes = subprocess.Popen(
+        "ps -eaf | grep " + name,
+        shell=True,
+        stdout=subprocess.PIPE
+    )
+    output = processes.stdout.read()
+    processes.stdout.close()
+    processes.wait()
+    return output
+
+
+def processRunning(name, path):
+    output = findProcess(name)
+    if re.search(path+name, output) is None:
+        return False
+    else:
+        return True
+
 
 class Spotify:
 
     def __init__(self):
         self.qdbus = 'qdbus org.mpris.MediaPlayer2.spotify / '
         'org.freedesktop.MediaPlayer2.'
+
+    def start(self, sleep):
+        os.system('(spotify 1>/dev/null 2>&1 &) && sleep %i' % sleep)
+
+    def exists(self):
+        path = '/opt/spotify/spotify-client/Data/'
+        name = 'SpotifyHelper'
+        return processRunning(name, path)
 
     def next(self):
         os.system(self.qdbus + 'Next')
@@ -26,3 +57,6 @@ class Spotify:
 
     def openuri(self, uri):
         os.system(self.qdbus + 'OpenUri %s' % uri)
+
+    def quit(self):
+        os.system(self.qdbus + 'Quit')
