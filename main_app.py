049d9e5378c0ea8bc139ba6a782d3f5576f29504
omnistegan <gmbouvier@gmail.com>
2015-05-13 17:02:38 -0700

    Added mpd support and tested functions across the platforms, may still be buggy in places.

--- main_app.py
+++ main_app.py
@@ -1,18 +1,25 @@
 import os
 import zbarfunc
+import album_search
 from spotify_qdbus import Spotify
-
-SPOTIFY = Spotify()
-os.system('spotify 1>/dev/null 2>&1 &')
-LAST_CODE = None
+from mpd_control import MPD_CLIENT
+from mpd_control import probe_playlists
 
 
 def qr_functions(last_code):
-    #Main scan and processing functions. Calls functions for QR scanning
+    """
+
+    Main scan and processing functions. Calls functions for QR scanning.
+    Takes the last code scanned as an input to prevent play inturruption
+    if the same code is scanned multiple times.
+
+    """
     print 'Scanning...'
+    MPD_CLIENT.send_idle()
     scanned_code = str(zbarfunc.QRCode().get_data())
     print scanned_code
-    allowed = [
+    media_functions = [
+        'artist_search',
         'next',
         'previous',
         'pause',
@@ -21,36 +28,58 @@ def qr_functions(last_code):
         'play',
         'break'
     ]
-    for each in allowed:
-        zbarfunc.save_qrcode(each, '00 ' + each)
-    if scanned_code != last_code and scanned_code in allowed:
-        if scanned_code == 'next':
+    for each in media_functions:
+        zbarfunc.save_qrcode(each, 'functions/' + each)
+    MPD_CLIENT.noidle()
+    if scanned_code != last_code and scanned_code in media_functions:
+        if scanned_code == 'artist_search':
+            album_search.artist_search_loop()
+        elif scanned_code == 'next':
             SPOTIFY.next()
+            MPD_CLIENT.next()
         elif scanned_code == 'previous':
             SPOTIFY.previous()
+            MPD_CLIENT.previous()
         elif scanned_code == 'pause':
             SPOTIFY.pause()
+            MPD_CLIENT.pause()
         elif scanned_code == 'playpause':
             SPOTIFY.play_pause()
+            MPD_CLIENT.pause()
         elif scanned_code == 'stop':
             SPOTIFY.stop()
+            MPD_CLIENT.clear()
         elif scanned_code == 'play':
             SPOTIFY.play()
+            MPD_CLIENT.play()
         elif scanned_code == 'break':
-            SPOTIFY.stop()
+            SPOTIFY.quit()
+            MPD_CLIENT.clear()
             return scanned_code
+        return 'function_used'
     elif scanned_code != last_code and scanned_code.startswith('spotify:'):
+        MPD_CLIENT.clear()
+        if SPOTIFY.exists() == False:
+            SPOTIFY.start(3)
         SPOTIFY.openuri(scanned_code)
         return scanned_code
+    elif scanned_code != last_code and scanned_code in probe_playlists():
+        SPOTIFY.quit()
+        MPD_CLIENT.load(scanned_code)
+        MPD_CLIENT.play()
+        return scanned_code
     else:
+        print 'No functions called. (Did you scan a valid code?)'
         return scanned_code
 
 
-while True:
-    try:
+if __name__ == '__main__':
+    SPOTIFY = Spotify()
+    LAST_CODE = None
+    while True:
         if LAST_CODE == 'break':
             break
         else:
             LAST_CODE = qr_functions(LAST_CODE)
-    except NameError:
-        LAST_CODE = qr_functions(None)
+
+
